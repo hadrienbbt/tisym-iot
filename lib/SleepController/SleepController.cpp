b87759b518c511b14b55ce8a82089cd9e3bd3ba1
Hadrien Barbat <hadrien@fedutia.fr>
2024-02-14 01:56:04 +0100

    match sleep GIOS with buttons macros

--- lib/SleepController/SleepController.cpp
+++ lib/SleepController/SleepController.cpp
@@ -1,32 +1,34 @@
 #include "SleepController.h"
 
-SleepController::SleepController(const uint64_t mask)
+SleepController::SleepController(const uint64_t mask, const uint8_t btn_0, const uint8_t btn_1, const uint8_t btn_2, const uint8_t btn_3, const uint8_t prog_btn)
 {
   this->mask = mask;
+  this->btn_0 = btn_0;
+  this->btn_1 = btn_1;
+  this->btn_2 = btn_2;
+  this->btn_3 = btn_3;
+  this->prog_btn = prog_btn;
 }
 
 void SleepController::setup(NetworkController *p_network)
 {
   uint64_t GPIO_reason_pow = esp_sleep_get_ext1_wakeup_status();
   double GPIO_reason = log(GPIO_reason_pow) / log(2);
-  if (GPIO_reason > 9)
+  if (GPIO_reason == btn_0 || GPIO_reason == btn_1 || GPIO_reason == btn_2 || GPIO_reason == btn_3)
   {
     Serial.println("\nGPIO that triggered the wake up: GPIO ");
     Serial.println(GPIO_reason, 0);
     p_network->publishButtonPressed(GPIO_reason);
     sleep();
   }
-  else if (GPIO_reason == 9)
+  if (GPIO_reason == prog_btn)
   {
     Serial.println("\nGPIO 9: starting OTA process...");
     Serial.println("OTA config starting...");
     return;
   }
-  else
-  {
-    Serial.println("Not awake due to GPIO");
-    sleep();
-  }
+  Serial.println("Not awake due to GPIO");
+  sleep();
 }
 
 void SleepController::sleep()
--- lib/SleepController/SleepController.h
+++ lib/SleepController/SleepController.h
@@ -6,9 +6,14 @@
 class SleepController
 {
   uint64_t mask;
+  uint8_t btn_0;
+  uint8_t btn_1;
+  uint8_t btn_2;
+  uint8_t btn_3;
+  uint8_t prog_btn;
 
 public:
-  SleepController(const uint64_t mask);
+  SleepController(const uint64_t mask, const uint8_t btn_0, const uint8_t btn_1, const uint8_t btn_2, const uint8_t btn_3, const uint8_t prog_btn);
   void setup(NetworkController *p_networkController);
 
   void sleep();
